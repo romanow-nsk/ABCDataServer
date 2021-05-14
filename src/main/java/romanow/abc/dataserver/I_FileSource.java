6c674e9857d717dc2d2ff85ab4562e217549c4d5
romanow-nsk <romanow@ngs.ru>
2021-05-14 13:58:02 +0700

    Сборка 0

--- src/main/java/romanow/abc/dataserver/I_FileSource.java
+++ src/main/java/romanow/abc/dataserver/I_FileSource.java
@@ -2,14 +2,13 @@ package romanow.abc.dataserver;
 
 import romanow.abc.core.UniException;
 import romanow.abc.core.entity.FileSource;
-import romanow.abc.ess2.core.entity.subjectarea.MetaFileModule;
 
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public interface I_FileSource {
-    public void start(DataServer db, MetaFileModule module, HashMap<String, String> params) throws UniException;
+    public void start(DataServer db, I_FileModuleDir module, HashMap<String, String> params) throws UniException;
     public void stop();
     public void tryToMake() throws UniException;
     public FileSource getNext() throws UniException;
