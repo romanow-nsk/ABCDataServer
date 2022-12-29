ea5d6a30d6088923e808c47acf9f75f8d1c28b07
romanow-nsk <romanow@ngs.ru>
2022-12-30 02:33:48 +0700

    Рефакторинг потоков и потоковых данных

--- src/main/java/romanow/abc/dataserver/LoopThread.java
+++ src/main/java/romanow/abc/dataserver/LoopThread.java
@@ -1,20 +1,29 @@
 package romanow.abc.dataserver;
 
 import lombok.Getter;
+import lombok.Setter;
 
 public class LoopThread{
-    private Thread loopThread=null; // Поток
-    private boolean stop=false;     // Двухфазное завершение
-    private  int delay;             // Интерфал цикла
-    private I_LoopBack back;        // События обратного вызова
-    @Getter private String name;    // Имя потока ()
+    private Thread loopThread=null;     // Поток
+    private boolean stop=false;         // Двухфазное завершение
+    private  int delay;                 // Интерфал цикла
+    private I_LoopBack back;    // События обратного вызова
+    private String name;        // Имя потока ()
+    public String getName() {
+        return name; }
+    public void setBack(I_LoopBack back) {
+        this.back = back; }
     public void shutdown(){
-        stop=true;                  // Двухфазное завершение
-        loopThread.interrupt();     // Прервать sleep
+        stop=true;                      // Двухфазное завершение
+        loopThread.interrupt();         // Прервать sleep
         }
     public LoopThread(String name0,int delayInSec, I_LoopBack loopBack){
         delay = delayInSec;
         back = loopBack;
+        name = name0;
+        }
+    public LoopThread(String name0,int delayInSec){
+        delay = delayInSec;
         name = name0;
         }
     public  void start(){
